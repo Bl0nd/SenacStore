b25d5a482a11802bdeb82e723d3bc4417756c2c6
Fernanda <feer_nanda18@hotmail.com>
2023-11-22 22:26:22 -0300

    produto

--- StoreApp/urls.py
+++ StoreApp/urls.py
@@ -3,4 +3,6 @@ from StoreApp import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('produtos/', views.produto_lista, name='produto_lista'),
+    path('produto/', views.produto_detalhe, name='produto_detalhes'),
 ]
--- StoreApp/views.py
+++ StoreApp/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from StoreApp.models import Departamento
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    departamentos = Departamento.objects.all()
+    context = {
+        'departamento' : departamentos
+    }
+    return render(request, 'index.html', context)
+
+def produto_lista(request):
+    return render(request, 'produtos.html')
+
+def produto_detalhe(request):
+    return render(request, 'produto_detalhes.html')
