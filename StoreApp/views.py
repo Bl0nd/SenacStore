c26568b13295167e75a2a1a979f60544ecb1172e
Fernanda <feer_nanda18@hotmail.com>
2023-11-29 20:17:19 -0300

    at

--- StoreApp/views.py
+++ StoreApp/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
-from StoreApp.models import Departamento
+from StoreApp.models import Departamento, Produto
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def produto_lista(request):
-    return render(request, 'produtos.html')
+   #Buscando produto no banco
+   produtos = Produto.objects.all()
+
+   context = {
+       'produtos' : produtos,
+       'categoria' : 'Todos Produtos'
+   }
+   return render(request, 'produtos.html', context)
 
 def produto_detalhe(request):
     return render(request, 'produto_detalhes.html')
