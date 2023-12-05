1d5342ec7ef662b117fe61e42499f440eef82c68
Fernanda <feer_nanda18@hotmail.com>
2023-12-04 22:01:56 -0300

    sobre a empresa

--- StoreApp/urls.py
+++ StoreApp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('produtos/', views.produto_lista, name='produto_lista'),
     path('produtos/<int:id>', views.produto_lista_por_id, name='produto_lista_por_id'),
     path('produto/<int:id>', views.produto_detalhe, name='produto_detalhes'),
+    path('sobre-empresa/', views.sobre_empresa, name='sobre_empresa')
 ]
--- StoreApp/views.py
+++ StoreApp/views.py
@@ -34,8 +34,13 @@ def produto_lista_por_id(request, id):
 
 def produto_detalhe(request, id):
     produto = Produto.objects.get(id = id)
+    produtos_relacionados = Produto.objects.filter(departamento_id = produto.departamento.id).exclude(id = id)[:4]
 
     context = {
-        'produto' : produto
+        'produto' : produto,
+        'produtos_relacionados' : produtos_relacionados
     }
     return render(request, 'produto_detalhes.html', context)
+
+def sobre_empresa(request):
+    return render(request, 'sobre_empresa.html')
